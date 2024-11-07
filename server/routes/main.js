6fe1a103b61c5c4df9ac7968d2146f2985b62148
absra47 <absragirma630@gmail.com>
2024-11-07 12:49:34 +0300

    styling the home page

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 //Routes
 router.get("", (req, res) => {
-  res.send("Hello World");
+  const locals = {
+    title: "Node js Blog",
+    description: "Simple blog created with Nodejs,Express & MongoDb.",
+  };
+  res.render("index", { locals });
+});
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+router.get("/contact", (req, res) => {
+  res.render("contact");
 });
 
 module.exports = router;
