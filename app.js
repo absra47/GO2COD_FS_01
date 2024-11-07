748170abd041624a2d9e64d7664eb0d96f8a7501
absra47 <absragirma630@gmail.com>
2024-11-07 18:00:33 +0300

    listing posts and routes  , pagination

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const expessLayout = require("express-ejs-layouts");
+
+const connectDB = require("./server/config/db");
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+//connect DB
+connectDB();
 
 app.use(express.static("public"));
 
